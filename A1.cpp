cd90e5ab9a0a9a72ae6145aae65a9417b91c0329
Shreya Awasthi <49202138+Shreya-awasthi@users.noreply.github.com>
2019-04-02 22:29:33 +0530

    Add files via upload

---
+++ A1.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+int min_at(int at[],int n)
+{
+	int index;
+	int temp = at[0];
+	for(int i=0; i<n; i++) 
+	{
+		if(temp>at[i]) 
+		{
+        	temp=at[i];
+        	index =i;
+      	}
+   	}
+   return index;
+}
+
+int main()
+{
+	int pro,i,at[10],bt[10];
+	cout<<"Enter number of processes:\n";
+	cin>>pro;
+	cout<<"Enter arrival time for all processes:\n";
+	for(i=0;i<pro;i++)
+	{
+		cin>>at[i];
+	}
+	for(i=0;i<pro;i++)
+	{
+		cout<<"Arrival time for process P"<<i<<" is: "<<at[i]<<endl;
+	}
+	cout<<"Enter burst time for all processes:\n";
+	for(i=0;i<pro;i++)
+	{
+		cin>>bt[i];
+	}
+	for(i=0;i<pro;i++)
+	{
+		cout<<"Burst time for process P"<<i<<" is: "<<bt[i]<<endl;
+	}
+	
+	
+	//Round robin for 3 units 
+	cout<<"After applying interruption after 3 time units for one process:\n";
+	int res=min_at(at,pro);
+	for(i=0;i<pro;i++)
+	{
+		if(i==res)
+		{
+			bt[i]=bt[i]-3;
+			
+		}
+		cout<<"Burst time for process P"<<i<<" is: "<<bt[i]<<endl;
+	}
+	
+	
+	cout<<endl<<"After 3 burst time is:\n"<<endl<<endl;
+	for(int i=0;i<pro;i++)
+	{
+		cout<<"Burst time for process P"<<i<<" is: "<<bt[i]<<endl;
+	}
+}
